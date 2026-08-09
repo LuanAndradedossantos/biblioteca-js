6c5742ed854567758eb1d58de93a3ea3d543bdd2
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-08-09 00:08:15 -0300

    feat: Adicionado a logica da classe listar disponiveis

---
+++ listarDisponiveis.js
@@ -0,0 +1,15 @@
+import { dadosLivros } from './dadosLivros.js';
+
+export function listarDisponiveis() {
+  console.log("\n=== LIVROS DISPONÍVEIS ===");
+  const disponiveis = dadosLivros.filter((livro) => livro.disponivel === true);
+
+  if (disponiveis.length === 0) {
+    console.log("Nenhum livro disponível no momento.");
+    return;
+  }
+
+  disponiveis.forEach((livro) => {
+    
+  });
+}
